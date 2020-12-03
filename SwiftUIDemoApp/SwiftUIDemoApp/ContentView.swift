607a665379e42034d1b8b1d4eb2f7dc89c4e8baa
倪申雷 <nishenlei@huami.com>
2020-12-03 19:59:36 +0800

    初始化APP

---
+++ SwiftUIDemoApp/SwiftUIDemoApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+// ContentView.swift
+// Created on 2020/12/3
+// Description <#⽂文件描述#> 
+// Copyright © 2020 SLNI inc. All rights reserved. 
+// @author slni
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemoApp/SwiftUIDemoApp/SwiftUIDemoAppApp.swift
@@ -0,0 +1,18 @@
+//
+// SwiftUIDemoAppApp.swift
+// Created on 2020/12/3
+// Description <#⽂文件描述#> 
+// Copyright © 2020 SLNI inc. All rights reserved. 
+// @author slni
+
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
