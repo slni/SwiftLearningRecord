35f529c01cdc0c2eb21739eb44a9948236f643b1
倪申雷 <nishenlei@huami.com>
2020-12-03 19:55:21 +0800

    Initial Commit

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+// ViewController.swift
+// Created on 2020/12/3
+// Description <#⽂文件描述#> 
+// Copyright © 2020 SLNI inc. All rights reserved. 
+// @author slni
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DemoAppTests/DemoAppTests.swift
@@ -0,0 +1,34 @@
+//
+// DemoAppTests.swift
+// Created on 2020/12/3
+// Description <#⽂文件描述#> 
+// Copyright © 2020 SLNI inc. All rights reserved. 
+// @author slni
+
+
+import XCTest
+@testable import DemoApp
+
+class DemoAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
